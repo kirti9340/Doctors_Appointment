52250692bdb7d043c3d24e376f28037ad7bc4487
Kirti <kirti9340@email.com>
2025-11-16 00:47:43 +0530

    updated code

--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { assets } from '../assets/assets'
 
 function MyProfile() {
+
+  const  [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: 'richardjameswap@gmail.com',
+    phone: '+1 123 456 7890',
+    address:{
+      line1:"57th Cross, Richmond",
+      line2: "Circle, Chunch Road, Londan"
+    },
+    gender:'Male',
+    dob:'2000-01-20'
+  })
+
+  const [isEdit, setIaEdit] = useState(true)
+
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt=""/>
+
+      {
+        isEdit
+        ? <input type="text" value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))}/>
+        :<p>{userData.name}</p>
+      }
+
+      <hr/>
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+           {
+              isEdit
+              ? <input type="text" value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone:e.target.value}))} />
+              :<p>{userData.phone}</p>
+           }
+          <p>Address:</p>
+          {
+            isEdit
+            ?<p>
+              <input onChange={(e) => setUserData(prev => ({...prev, address:{...prev.address, line1: e.target.value}}))} type="text"/>
+              <br/>
+              <input type="text"/>
+            </p>
+            :<p>
+              {userData.address.line1}
+              <br/>
+              {userData.address.line2}
+            </p>
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
